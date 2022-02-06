be1687c6aea2f65e9502870a8bd58592abd66d6d
Arnold Gerry <arnaud.gerry@gmail.com>
2022-02-07 01:32:46 +0300

    ts - check functionality of the script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 function check(){
-    document.write("It is working")
-}
+   var myScore=0;
+   var questionOne=document.quiz.quiz1.value;
+   var questionTwo=document.quiz.quiz2.value;
+   var questionThree=document.quiz.quiz3.value;
+   var questionFour=document.quiz.quiz4.value;
+   var questionFive=document.quiz.quiz5.value;
+   var questionSix=document.quiz.quiz16value;
+   var questionSeven=document.quiz.quiz7.value;
+   var questionEight=document.quiz.quiz8.value;
+   var questionNine=document.quiz.quiz9.value;
+   var questionTen=document.quiz.quiz10.value;
+   
+   if (questionOne=="Float")  {myScore++}
+   if (questionTwo=="Both")  {myScore++}
+   if (questionThree=="Both")  {myScore++}
+   if (questionFour=="Ignores")  {myScore++}
+   if (questionFive=="all")  {myScore++}
+   if (questionSix=="Object")  {myScore++}
+   if (questionSeven=="false")  {myScore++}
+   if (questionEight=="Number")  {myScore++}
+   if (questionNine=="All")  {myScore++}
+   if (questionTen=="Cassandra")  {myScore++}
+
+   document.write(myScore);
+} 
